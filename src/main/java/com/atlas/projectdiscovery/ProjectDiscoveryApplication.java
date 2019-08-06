e7d157c08b01dd87e00bfa9a178267c8d26fc7c0
Peter Fajardo <peterfajardo@gmail.com>
2019-08-06 14:23:10 -0700

    first commit

---
+++ src/main/java/com/atlas/projectdiscovery/ProjectDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.atlas.projectdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlas/projectdiscovery/ProjectDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.projectdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
